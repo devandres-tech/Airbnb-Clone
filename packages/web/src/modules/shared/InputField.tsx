789bc14aafd664a7b9526ca371cd513e0b16d7e1
andres alcocer <andresalcocer7@yahoo.com>
2019-07-19 23:34:04 -0400

    created generic formik input field

---
+++ packages/web/src/modules/shared/InputField.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { FieldProps } from 'formik';
+import { Form, Input, } from 'antd';
+
+
+export const InputField: React.SFC<FieldProps<any> & {}> = ({
+  field, // { name, value, onChange, onBlur }
+  form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  ...props
+}) => {
+  const errorMessage = touched[field.name] && errors[field.name];
+
+  return (
+    <Form.Item
+      help={errorMessage}
+      validateStatus={
+        errorMessage ? 'error' : undefined
+      }
+    >
+      <Input
+        {...field}
+        {...props}
+      />
+    </Form.Item>
+
+  )
+} 
