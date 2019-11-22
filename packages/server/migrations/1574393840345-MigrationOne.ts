5d9c763731502f5a9d7e2eb7bd121ce017c68cb0
andres alcocer <andresalcocer7@yahoo.com>
2019-11-21 22:39:39 -0500

    created migration file

---
+++ packages/server/migrations/1574393840345-MigrationOne.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class migrationOne1574393840345 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
