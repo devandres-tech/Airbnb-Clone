2e16c42cccc1d5a0e0f5853855a5e3cbd1874603
andres alcocer <andresalcocer7@yahoo.com>
2019-12-01 20:56:10 -0500

    created delete lising mutation

---
+++ packages/server/src/modules/listing/delete/resolvers.ts
@@ -0,0 +1,30 @@
+import { ResolverMap } from '../../../types/graphql-utils';
+import { Listing } from '../../../entity/Listing';
+
+export const resolvers: ResolverMap = {
+  Mutation: {
+    deleteListing: async (_, { id }, { session }) => {
+      console.log("SESSION IS: ", session)
+      if (!session.userId) {
+        throw new Error("not authenticated");
+      }
+
+      // find base entity
+      const listing = await Listing.findOne({ where: { id } })
+
+      if (!listing) {
+        throw new Error("does not exist");
+      }
+
+      // check for ownership
+      if (session.userId !== listing.userId) {
+        throw new Error("not authorized");
+      }
+
+      await Listing.remove(listing)
+
+      return true;
+    }
+  }
+};
+
