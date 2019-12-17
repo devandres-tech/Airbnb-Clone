1638667b5b4c0df5fcc0ae95bb6a0e6ddb029ad4
andres alcocer <andresalcocer7@yahoo.com>
2019-12-16 21:38:22 -0500

    implemented auth route logic

--- packages/controller/src/modules/auth/AuthRoute.tsx
+++ packages/controller/src/modules/auth/AuthRoute.tsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
-import { RouteProps } from 'react-router';
-import { graphql } from 'react-apollo';
+import { RouteProps, Route, Redirect } from 'react-router';
+import { graphql, ChildProps } from 'react-apollo';
 import gql from 'graphql-tag';
+
+import { MeQuery } from '../../schemaTypes';
+import { RouteComponentProps } from 'react-router-dom';
+
+type Props = RouteProps;
 
 const meQuery = gql`
   query MeQuery {
@@ -11,14 +16,29 @@ const meQuery = gql`
   }
 `
 
-class C extends Component<RouteProps> {
-  render() {
-    return (
-      <div>
+class C extends Component<ChildProps<Props, MeQuery>> {
+  renderRoute = (routeProps: RouteComponentProps<{}>) => {
+    const { data, component } = this.props;
 
-      </div>
-    )
+    if (!data || !data.loading) {
+      // loading screen
+      return null;
+    }
+
+    // data has loaded
+    if (!data.me || !data.me.email) {
+      // user not logged in
+      return <Redirect to="/login" />
+    }
+
+    const Component = component as any;
+    return <Component {...routeProps} />;
+  }
+
+  render() {
+    const { data: _, component: __, ...rest } = this.props;
+    return <Route {...rest} render={this.renderRoute} />;
   }
 }
 
-export const AuthRoute = graphql(meQuery)(C);
+export const AuthRoute = graphql<Props, MeQuery>(meQuery)(C);
