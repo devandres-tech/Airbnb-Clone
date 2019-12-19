84f7596db79381979268fc1378cb4e320d4f64bf
andres alcocer <andresalcocer7@yahoo.com>
2019-12-18 23:59:51 -0500

    implemented create listing form

--- packages/web/src/modules/listing/create/CreateListingConnector.tsx
+++ packages/web/src/modules/listing/create/CreateListingConnector.tsx
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Form, Icon, Button } from 'antd';
+import { withFormik, FormikProps, Field, Form as FForm, Formik } from 'formik'
+import { InputField } from '../../shared/InputField';
+import { Link } from 'react-router-dom';
+
+// name: String!
+// category: String!
+// description: String!
+// price: Int!
+// beds: Int!
+// guests: Int!
+// latitude: Float!
+// longitude: Float!
+// amenities: [String!]!
 
 export default class CreateListingConnector extends Component {
   render() {
     return (
-      <div>
-        <h1>Secret Route!!!</h1>
-      </div>
+      <Formik>
+        {
+          () => (
+            <FForm className="login-form">
+              <Field
+                name="email"
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="email"
+                component={InputField}
+              />
+              <Field
+                name="password"
+                type="password"
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="password"
+                component={InputField}
+              />
+              </Form.Item>
+            <Form.Item>
+              <Link to="/register">register</Link>
+            </Form.Item>
+            </FForm>
+    )
+  }
+      </Formik>
     )
   }
 }
