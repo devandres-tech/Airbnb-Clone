137ab4c13e4ff2273b6ee9631c9cb81684cac67b
andres alcocer <andresalcocer7@yahoo.com>
2019-12-20 17:32:07 -0500

    implementednext page logic

--- packages/web/src/modules/listing/create/CreateListingConnector.tsx
+++ packages/web/src/modules/listing/create/CreateListingConnector.tsx
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
 import { Form, Icon, Button } from 'antd';
 import { withFormik, FormikProps, Field, Form as FForm, Formik } from 'formik'
-import { InputField } from '../../shared/InputField';
-import { Link } from 'react-router-dom';
 import { Page1 } from './view/Page1';
 import { Page2 } from './view/Page2';
 import { Page3 } from './view/Page3';
 
-interface FormValues {
-  name: string;
-  category: string;
-  description: string;
-  price: number;
-  beds: string;
-  guests: string;
-  latitude: string;
-  longitude: string;
-  amenities: string[];
+interface State {
+  page: number
 }
 
 const InitialValues = {
@@ -33,7 +23,7 @@ const InitialValues = {
 
 const pages = [<Page1 />, <Page2 />, <Page3 />];
 
-export default class CreateListingConnector extends Component {
+export default class CreateListingConnector extends Component<{}, State> {
   state = {
     page: 0
   }
@@ -42,19 +32,30 @@ export default class CreateListingConnector extends Component {
     console.log('values: ', values);
   }
 
+  nextPage = () => this.setState(state => ({ page: state.page + 1 }))
+
   render() {
     return (
       <Formik initialValues={InitialValues} onSubmit={this.submit}>
         {
           () => (
             <FForm className="login-form">
+              {pages[this.state.page]}
               <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                >
-                  Create Listing
-                </Button>
+                {
+                  this.state.page === pages.length - 1 ? (
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                    >
+                      Create Listing
+                    </Button>
+                  ) : (
+                      <Button type="primary" onClick={this.nextPage}>
+                        Next Page
+                      </Button>
+                    )
+                }
               </Form.Item>
             </FForm>
           )
